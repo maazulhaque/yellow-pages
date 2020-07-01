10bbea700d2b5f5eb9ed3f074f0038786748512c
Maaz <maaz@fundthrough.com>
2020-07-01 19:34:10 -0400

    schema commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_232218) do
+ActiveRecord::Schema.define(version: 2020_07_01_233321) do
+
+  create_table "headings", force: :cascade do |t|
+    t.string "heading"
+    t.integer "member_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_headings_on_member_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_07_01_232218) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "headings", "members"
 end
