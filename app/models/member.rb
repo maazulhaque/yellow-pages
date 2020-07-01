c7ea147f5fe5e033a0402850805b321bcfba5b78
Maaz <maaz@fundthrough.com>
2020-07-01 19:59:28 -0400

    bi directional self referential relationship added

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,12 @@
 class Member < ApplicationRecord
+  has_many :headings
+  has_many :friendships, :foreign_key => "person_id", :class_name => "Friendship"
+  has_many :friends, :through => :friendships
+
+
+  def befriend(member)
+    # TODO: put in check that association does not exist
+    self.friends << member
+    member.friends << self
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_233321) do
+ActiveRecord::Schema.define(version: 2020_07_01_235545) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "person_id"
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "headings", force: :cascade do |t|
     t.string "heading"
