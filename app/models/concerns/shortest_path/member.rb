ceeda20c3d9e68332a664ed85c3b020b47ed0d97
Maaz <maaz@fundthrough.com>
2020-07-03 16:23:12 -0400

    tidy up

--- app/models/concerns/shortest_path/member.rb
+++ app/models/concerns/shortest_path/member.rb
@@ -3,30 +3,39 @@ module ShortestPath
     extend ActiveSupport::Concern
 
     def shortest_path(member)
-      shortest_paths = {self: nil}
-      queue = [self]
-      visited = Set.new
+      @shortest_paths = {self: nil}
+      @queue = [self]
+      @visited = Set.new
+      @current_member = nil
 
-      while queue.present? do
-        current_member = queue.shift
-        break if current_member == member
-        visited.add(current_member)
-        friends = current_member.friends
+      while @queue.present? do
+        @current_member = @queue.shift
+        break if @current_member == member
+        @visited.add(@current_member)
+        friends = @current_member.friends
         return 'outside of your network' unless friends.present?
         friends.each do |friend|
-          if !visited.include?(friend)
-            shortest_paths[friend] = current_member
-            queue.push(friend)
+          if !@visited.include?(friend)
+            @shortest_paths[friend] = @current_member
+            @queue.push(friend)
           end
         end
       end
-      return 'outside of your network' unless current_member
-      path = [current_member.name]
-      current_member = shortest_paths[current_member]
-      while current_member do
-        path.push('->' + current_member.name)
-        next_member = shortest_paths[current_member]
-        current_member = next_member
+      generate_path
+
+    end
+
+    protected
+
+    def generate_path
+      return 'outside of your network' unless @current_member
+
+      path = [@current_member.name]
+      @current_member = @shortest_paths[@current_member]
+      while @current_member do
+        path.push('->' + @current_member.name)
+        next_member = @shortest_paths[@current_member]
+        @current_member = next_member
       end
       path.join
     end
